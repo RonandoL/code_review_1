6081b44d4de6ca5d47c1faa25ff8112e70f26190
Ronando <student@epicodus.com>
2015-08-07 11:24:28 -0700

    Created new spec, counts one word in sentence

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,10 +1,17 @@
 require('pry')
 
 class String
-  define_method(:counting_words) do
-    sentence = self
-    sentence_array = []
+  define_method(:counting_words) do |word|
+    sentence = self                   # inputted sentence
+    count = 0
+    sentence_array = []               # create empty array for string input
+    sentence_array = sentence.split() # creates array of strings
 
-    sentence_array = sentence.split()
+    sentence_array.each do |i|   # iterate through array w/argument
+      if i == word
+        count += 1
+      end
+    end
+    count
   end
 end
