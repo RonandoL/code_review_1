df702887e1a9b9c8d3228d49eae9583a18881f7b
Ronando <student@epicodus.com>
2015-08-07 09:36:26 -0700

    Changed 1st spec, started over, putting words into array

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -3,12 +3,12 @@ require('word_count')
 
 describe('String#counting_words') do
 
-  it('put a word into an array') do
-    expect(("batman").counting_words).to(eq(["b", "a", "t", "m", "a", "n"]))
+  it('put multiple words into an array of individual words') do
+    expect(("the dog is red").counting_words).to(eq(["the", "dog", "is", "red"]))
   end
 
-  it('identify if a letter is in the array') do
-    expect(("a").counting_words).to(eq("true"))
-  end
+  # it('identify if a letter is in the array') do
+  #   expect(("a").counting_words).to(eq("true"))
+  # end
 
 end
