1137ab2e19b5d3ae1c4fcf7e448328078a733675
Ronando <student@epicodus.com>
2015-08-07 09:31:20 -0700

    2nd spec failed as expected.

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -7,4 +7,8 @@ describe('String#counting_words') do
     expect(("batman").counting_words).to(eq(["b", "a", "t", "m", "a", "n"]))
   end
 
+  it('identify if a letter is in the array') do
+    expect(("a").counting_words).to(eq("true"))
+  end
+
 end
